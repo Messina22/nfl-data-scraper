bb644ea6ce13e0239dc0b0d0d330fa58197fd059
Nick Messina <nicknmessina@gmail.com>
2024-07-31 00:58:03 -0500

    chore: connect to nfl.com

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Make HTTP request
+	response, err := http.Get("https://www.nfl.com/scores/2017/reg1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	// Create a goquery document from the HTTP response
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body. ", err)
+	}
+
+	// Find and print all links
+	document.Find("a").Each(func(index int, element *goquery.Selection) {
+		href, exists := element.Attr("href")
+		if exists {
+			fmt.Println(href)
+		}
+	})
 }
+
